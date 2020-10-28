ad1a25e9e592c5cf8868ff1f80b9a8a716159d2f
melroy-b <melroybarreto61@gmail.com>
2020-10-28 12:32:45 +0530

    Minor fixes

--- app/components/image.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-import {action} from '@ember/object';
-import {tracked} from '@glimmer/tracking'
- 
-export default class ImageComponent extends Component {
-    
-}
